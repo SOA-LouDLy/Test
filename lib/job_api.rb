db488783c630da2698ebffa265faaa71ce83c8ed
Thierry <marcelinthierry@gmail.com>
2021-10-27 22:22:40 +0800

    CareerJet Gem API

---
+++ lib/job_api.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require_relative 'job'
+require 'careerjet/api_client'
+
+module Jobify
+  # Library for CareerJet Gem API
+  class CarrerjetApi
+    def initialize(id)
+      @id = id
+    end
+
+    # def job(skill, location, contracttype = 'none', contractperiod = 'none')
+    def job(skill, location)
+      job = Request.new(@id, skill, location).job
+      Job.new(job)
+    end
+  end
+
+  # Sends request to the Gem API
+  class Request
+    # def initialize(key, skill, location, contracttype = 'none', contractperiod = 'none')
+    def initialize(key, skill, location)
+      @key = key
+      @skill = skill
+      @location = location
+      #   @contracttype = contracttype
+      #   @contractperiod = contractperiod
+    end
+
+    def job
+      cj_api_client = Careerjet::APIClient.new(locale: :en_US)
+      cj_api_client.search(
+        keywords: @skill,
+        location: @location,
+        affid: @key,
+        user_ip: '11.22.33.44',
+        user_agent: 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0',
+        url: 'http://www.example.com/jobsearch?q=ruby&l=london',
+        sort: 'date'
+        # contracttype: 'p',
+        # contractperiod: 'p'
+      ).jobs.first
+    end
+  end
+end
