2933694b9c7bc640a56cc1f0b34c1d9a8e04ca06
Malherbe <malherbe.billy@gmail.com>
2021-10-30 00:30:47 +0800

    gateways entities mappers

---
+++ lib/gateways_affinda/affinda_api.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'httmultiparty'
+require 'yaml'
+require 'pdf-reader'
+
+config = YAML.safe_load(File.read('../config/secrets.yml'))
+
+class ResumeParser
+  include HTTMultiParty
+  base_uri 'https://api.affinda.com/v1'
+end
+
+response = ResumeParser.post('/', :query => {
+    :foo      => 'bar',
+    :somefile => File.new('README.md')
+  })
