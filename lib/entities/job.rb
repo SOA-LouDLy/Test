a00490f8a443f530dcad69de83442330432031d3
Loudwige Odice <55907221+Gaby077@users.noreply.github.com>
2021-10-30 14:08:18 +0800

    gateway-datamapper-entity design patterns refactoring

--- lib/entities/job.rb
+++ lib/entities/job.rb
@@ -1,1 +1,18 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
+
+require 'dry-types'
+require 'dry-struct'
+
+module Jobify
+  module CareerJet
+    class Job < Dry::Struct
+      include Dry.Types
+      attribute :date, Strict::String
+      attribute :url, Strict::String
+      attribute :title, Strict::String
+      attribute :description, Strict::String
+      attribute :company, Strict::String
+      attribute :locations, Strict::String
+    end
+  end
+end
